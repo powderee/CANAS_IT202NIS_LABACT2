ba27c268fe7b8fbde68692da18da4fcd49147dfd
C302-39 <C302-39@C302-39>
2026-02-07 11:52:53 +0800

    Add project files.

---
+++ MATHCSHARPERS/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace MATHCSHARPERS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Num_1 = new System.Windows.Forms.Label();
+            this.Num_2 = new System.Windows.Forms.Label();
+            this.Num_3 = new System.Windows.Forms.Label();
+            this.Num_4 = new System.Windows.Forms.Label();
+            this.Num_5 = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(108, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(108, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(108, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.Location = new System.Drawing.Point(108, 193);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.White;
+            this.textBox5.Location = new System.Drawing.Point(108, 242);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // Num_1
+            // 
+            this.Num_1.AutoSize = true;
+            this.Num_1.BackColor = System.Drawing.Color.MistyRose;
+            this.Num_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_1.Location = new System.Drawing.Point(20, 38);
+            this.Num_1.Name = "Num_1";
+            this.Num_1.Size = new System.Drawing.Size(71, 15);
+            this.Num_1.TabIndex = 5;
+            this.Num_1.Text = "1st Number";
+            this.Num_1.Click += new System.EventHandler(this.Num_1_Click);
+            // 
+            // Num_2
+            // 
+            this.Num_2.AutoSize = true;
+            this.Num_2.BackColor = System.Drawing.Color.MistyRose;
+            this.Num_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_2.Location = new System.Drawing.Point(18, 89);
+            this.Num_2.Name = "Num_2";
+            this.Num_2.Size = new System.Drawing.Size(76, 15);
+            this.Num_2.TabIndex = 6;
+            this.Num_2.Text = "2nd Number";
+            this.Num_2.Click += new System.EventHandler(this.Num_2_Click);
+            // 
+            // Num_3
+            // 
+            this.Num_3.AutoSize = true;
+            this.Num_3.BackColor = System.Drawing.Color.MistyRose;
+            this.Num_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_3.Location = new System.Drawing.Point(19, 139);
+            this.Num_3.Name = "Num_3";
+            this.Num_3.Size = new System.Drawing.Size(73, 15);
+            this.Num_3.TabIndex = 7;
+            this.Num_3.Text = "3rd Number";
+            this.Num_3.Click += new System.EventHandler(this.Num_3_Click);
+            // 
+            // Num_4
+            // 
+            this.Num_4.AutoSize = true;
+            this.Num_4.BackColor = System.Drawing.Color.MistyRose;
+            this.Num_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_4.Location = new System.Drawing.Point(20, 193);
+            this.Num_4.Name = "Num_4";
+            this.Num_4.Size = new System.Drawing.Size(72, 15);
+            this.Num_4.TabIndex = 8;
+            this.Num_4.Text = "4th Number";
+            this.Num_4.Click += new System.EventHandler(this.Num_4_Click);
+            // 
+            // Num_5
+            // 
+            this.Num_5.AutoSize = true;
+            this.Num_5.BackColor = System.Drawing.Color.MistyRose;
+            this.Num_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_5.Location = new System.Drawing.Point(20, 242);
+            this.Num_5.Name = "Num_5";
+            this.Num_5.Size = new System.Drawing.Size(72, 15);
+            this.Num_5.TabIndex = 9;
+            this.Num_5.Text = "5th Number";
+            this.Num_5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.Color.MistyRose;
+            this.Submit.Location = new System.Drawing.Point(108, 306);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(100, 23);
+            this.Submit.TabIndex = 10;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.MistyRose;
+            this.Clear.Location = new System.Drawing.Point(239, 306);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(100, 23);
+            this.Clear.TabIndex = 11;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.MistyRose;
+            this.Exit.Location = new System.Drawing.Point(372, 306);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(100, 23);
+            this.Exit.TabIndex = 12;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MistyRose;
+            this.label1.Location = new System.Drawing.Point(472, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.MistyRose;
+            this.label2.Location = new System.Drawing.Point(551, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Results";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.Num_5);
+            this.Controls.Add(this.Num_4);
+            this.Controls.Add(this.Num_3);
+            this.Controls.Add(this.Num_2);
+            this.Controls.Add(this.Num_1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label Num_1;
+        private System.Windows.Forms.Label Num_2;
+        private System.Windows.Forms.Label Num_3;
+        private System.Windows.Forms.Label Num_4;
+        private System.Windows.Forms.Label Num_5;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MATHCSHARPERS/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MATHCSHARPERS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            //def variable for later use
+            double Num_1, Num_2, Num_3, Num_4, Num_5;
+
+            double.TryParse(textBox1.Text, out Num_1);
+            double.TryParse(textBox2.Text, out Num_2);  
+            double.TryParse(textBox3.Text, out Num_3);
+            double.TryParse(textBox4.Text, out Num_4);  
+            double.TryParse(textBox5.Text, out Num_5);
+
+            //spam math sheet
+            double Absolute , Absolute2 , Absolute3 , Absolute4 , Absolute5 ;
+
+            Absolute = Math.Abs(Num_1); 
+            Absolute2 = Math.Abs(Num_2);
+            Absolute3 = Math.Abs(Num_3);
+            Absolute4 = Math.Abs(Num_4);
+            Absolute5 = Math.Abs(Num_5);
+
+            double Sqrt, Sqrt2, Sqrt3, Sqrt4, Sqrt5;
+
+            Sqrt = Math.Sqrt(Num_1);
+            Sqrt2 = Math.Sqrt(Num_2);
+            Sqrt3 = Math.Sqrt(Num_3);
+            Sqrt4 = Math.Sqrt(Num_4);   
+            Sqrt5 = Math.Sqrt(Num_5);
+
+            double floor, floor2, floor3, floor4, floor5;
+
+            floor = Math.Floor(Num_1);
+            floor2 = Math.Floor(Num_2);
+            floor3 = Math.Floor(Num_3);
+            floor4 = Math.Floor(Num_4);
+            floor5 = Math.Floor(Num_5);
+
+            double Max ,  Max2 , Max3 , RealMax;
+
+            double Min , Min2 , Min3 , RealMin;
+
+            Max = Math.Max(Num_1, Num_2);
+            Max2 = Math.Max(Max, Num_3);
+            Max3 = Math.Max(Max2, Num_4);
+            RealMax = Math.Max(Max3, Num_1);
+
+            Min = Math.Min(Num_1 , Num_2);
+            Min2 = Math.Min(Min, Num_3);
+            Min3 = Math.Min(Min2 , Num_4);
+            RealMin = Math.Min(Min3 , Num_5);
+
+
+            label1.Text = ($"Absolute Value: {Absolute} , {Absolute2} , {Absolute3} , {Absolute4} , {Absolute5} \nSquare Roots: {Sqrt} , {Sqrt2} , {Sqrt3} , {Sqrt4} , {Sqrt5}\n" +
+                $"Floor Values: {floor} , {floor2} , {floor3} , {floor4} , {floor5}\nMaximum Value:{RealMax}\nMinimum Value:{RealMin}").ToString();
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            label1.Text = ("");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
